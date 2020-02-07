2b2df131cd0373f3df138c827f35deb715a28b1d
Omid Mirab <48311220+OmidMirab@users.noreply.github.com>
2020-02-07 22:41:36 +0330

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+client.on('message', message => {
+    if (message.content === 'ping') {
+       message.reply('pong');
+       }
+});
+client.login(Njc1MzI2NTk4NDY3Mjg5MTI4.Xj10LA.oOcJ0pnjnfREJPUGRaOPqMBA0h4);
