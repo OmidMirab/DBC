02df370e001f6461b1b471cbbfef0daa1f5706f0
Omid Mirab <48311220+OmidMirab@users.noreply.github.com>
2020-02-07 23:15:37 +0330

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,11 +1,29 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
+
+ 
+
 client.on('ready', () => {
+
     console.log('I am ready!');
+
 });
+
+ 
+
 client.on('message', message => {
+
     if (message.content === 'ping') {
+
        message.reply('pong');
+
        }
+
 });
-client.login(Njc1MzI2NTk4NDY3Mjg5MTI4.Xj10LA.oOcJ0pnjnfREJPUGRaOPqMBA0h4);
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(Njc1MzI2NTk4NDY3Mjg5MTI4.Xj10LA.oOcJ0pnjnfREJPUGRaOPqMBA0h4);//BOT_TOKEN is the Client Secret
